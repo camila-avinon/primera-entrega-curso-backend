6f3e4e5d6a304e348be4d5e3b85decf64a1c31ff
camilaAvinon <camila.avignon@gmail.com>
2024-09-22 19:33:32 -0300

    Delete producto del carrito

--- src/routes/cart-routes.js
+++ src/routes/cart-routes.js
@@ -14,16 +14,22 @@ router.get('/:cId', async (req, res) => {
         }
         res.send(cart)
     }catch(e){
-        console.log(e)
+        console.log('Error al eliminar el producto: ', e)
+        res.status(500).json({error:'No se encontró el carrito', msg:e})
     }
 })
 
 // Crear carrito
 router.post('/', async (req, res) => {
     const {products} = req.body
-    if (!products) res.send({status: 'error', msg:"Carrito vacío"})
-    const cart = await cartModel.create({products})
-    if (cart) res.send({status: 'success', msg:"Carrito creado"})
+    try{
+        if (!products) res.send({status: 'error', msg:"Carrito vacío"})
+        const cart = await cartModel.create({products})
+        if (cart) res.send({status: 'success', msg:"Carrito creado"})
+    } catch(e){
+        console.log('Error al crear el carrito: ', e)
+        res.status(500).json({error:'Error al crear el carrito', msg:e})
+    }
 })
 
 // Agregar producto
@@ -31,10 +37,34 @@ router.post('/:cId/product/:pId', async(req, res) => {
     const {cId, pId} = req.params
     try{
         const updatedProduct = await cartModel.findById(cId)
-        const product = updatedProduct.products.find(p => p._id == pId)
+        const product = updatedProduct.products.find(p => p.product == pId)
         product.quantity += 1
         await updatedProduct.save()
         res.send({status: "Success", msg:"Producto agregado correctamente"})
+    }catch(e){
+        console.log('Error al agregar producto al carrito: ', e)
+        res.status(500).json({error:'Error al agregar producto al carrito', msg:e})
+    }
+})
+
+router.put('/:cId/products/:pId', async (req, res) => {
+    const {cId, pId} = req.params
+    const quantity = req.body.quantity
+    try{
+        const cart = await cartModel.findById(cId)
+        const product = cart.products.find(p => p.product == pId)
+        product.quantity = quantity
+        await cart.save()
+        res.send({status: "Success", msg:"Cantidad modificada correctamente", cart})
+    }catch(e){
+        console.log(e)
+    }
+})
+
+router.delete('/:cId', async (req, res) => {
+    const {cId} = req.params
+    try{
+        
     }catch(e){
         console.log(e)
     }
